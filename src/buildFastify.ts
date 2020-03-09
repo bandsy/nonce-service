e06ed8a3e4fd11714efc50ab08875c9969f1fa60
Dragon1320 <lukasznie1320@gmail.com>
2020-03-09 13:05:15 +0000

    mongodb setup?

--- src/buildFastify.ts
+++ src/buildFastify.ts
@@ -1,5 +1,46 @@
 import Fastify from "fastify";
 
+// disable unused vars until i get typescript 3.8 to work properly...
+// eslint-disable-next-line no-unused-vars
+import mongoose, { Document, Schema, model } from "mongoose";
+
+interface INonce {
+  name: string,
+  number?: number,
+}
+
+interface INonceDocument extends INonce, Document { }
+
+const NonceSchema = new Schema({
+  name: { type: String, required: true },
+  number: { type: Number, default: 23 },
+});
+
+const NonceModel = model<INonceDocument>("Nonce", NonceSchema);
+
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_PORT,
+  MONGO_DB,
+  MONGO_REPLICASET,
+} = process.env;
+
+const mongoUrl = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?replicaSet=${MONGO_REPLICASET}&authSource=admin`;
+
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+});
+
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+// eslint-disable-next-line no-console
+db.on("error", console.error);
+// eslint-disable-next-line no-console
+db.on("open", () => console.log("db conn"));
+
 const buildFastify = (settings = {}) => {
   const fastify = Fastify(settings);
 
@@ -8,6 +49,9 @@ const buildFastify = (settings = {}) => {
     gay: "cunty mcjim",
     aids: "yyyyyyyyy",
   }));
+
+  fastify.get("/nonce", async () => NonceModel.find());
+  fastify.post("/nonce", async (request) => new NonceModel(request.body).save());
 
   return fastify;
 };
